1cdc607ef50c53fa28399d582d7858354a04cc10
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-22 20:25:18 +0530

    created helper functions

---
+++ utils/helpers.js
@@ -0,0 +1,18 @@
+const NUMBER_MAP = require("./numberMap");
+
+function deStructureTimeIntoHoursAndMinutes(stdTimeFormat) {
+  return stdTimeFormat.split(":");
+}
+
+function convertNumberToEnglishString(numericTime) {
+  let result;
+  // ? ref to the array of keys in the dict
+  let timeInNumericArray = Object.keys(NUMBER_MAP);
+
+  // ? loop thru the array of time to extract it's associated english value
+  for (let i = 0; i < timeInNumericArray.length; i++) {
+    if (timeInNumericArray[i] === numericTime) {
+      return NUMBER_MAP[i];
+    }
+  }
+}
