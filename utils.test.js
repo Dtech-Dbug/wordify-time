a090ce49c97465f2148d1999e853878813629660
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-25 21:03:59 +0530

    checked numberMap

--- utils.test.js
+++ utils.test.js
@@ -3,6 +3,8 @@ const {
   convertNumberToEnglishString,
 } = require("./utils/helpers");
 
+const numberMap = require("./utils/numberMap");
+
 test('should time be split on ":"', () => {
   expect(deStructureTimeIntoHoursAndMinutes("6:30")).toStrictEqual(["6", "30"]);
 });
@@ -13,4 +15,12 @@ test('should time be split on ":"', () => {
 
 test('should time be split on ":"', () => {
   expect(deStructureTimeIntoHoursAndMinutes("9:15")).toStrictEqual(["9", "15"]);
+});
+
+test("number map", () => {
+  expect(numberMap[2]).toBe("Two");
+});
+
+test("number map", () => {
+  expect(numberMap[4]).toBe("Four");
 });
